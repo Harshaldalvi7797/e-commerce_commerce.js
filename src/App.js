d4a3221fcf17a30a6597dc783d28c1146a000f83
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-12-04 00:39:51 +0530

    commit : product component and show data through array

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+import Products from "./components/Products/Products";
+
+export default function App() {
+  return (
+    <div>
+      E-Commerce
+      <Products />
+    </div>
+  );
+}
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,25 @@
+/** @format */
+
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import Product from "./Product/Product";
+
+const products = [
+  { id: 1, name: "Shooes", description: "hello" },
+  { id: 2, name: "Shirts", description: "Awesome" }
+];
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map(product => (
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+export default Products;
